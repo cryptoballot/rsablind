f6e248a5bc2da786b7fee138270a9e4dcc6fa85c
phayes <patrick.d.hayes@gmail.com>
2016-01-02 17:45:39 -0800

    Initial commit - work in progress

---
+++ rsablind_test.go
@@ -0,0 +1,60 @@
+package rsablind
+
+import (
+"math/big"
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+	"github.com/davecgh/go-spew/spew"
+)
+
+func TestBlindSign(t *testing.T) {
+	//data := []byte("data")
+
+	//blindSignTest(t, "TestBlindSign", data);
+}
+
+func TestBlindSignBig(t *testing.T) {
+	c := 64
+	data := make([]byte, c)
+	_, err := rand.Read(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	blindSignTest(t, "TestBlindSignBig", data);
+}
+
+func blindSignTest(t *testing.T, test string, data []byte) {
+	key, _ := rsa.GenerateKey(rand.Reader, 512)
+	blinded, unblinder := Blind(&key.PublicKey, data)
+	
+
+	sig, err := BlindSign(key, blinded)
+	if err != nil {
+		t.Error(err)
+	}
+	unblindSig := Unblind(&key.PublicKey, sig, unblinder)
+
+	// DEBUG - get bitlens
+	spew.Dump(key.PublicKey.N.BitLen())
+	spew.Dump(new(big.Int).SetBytes(data).BitLen())
+	//spew.Dump(new(big.Int).SetBytes(unblindSig).BitLen())
+
+
+	// Check to make sure both the blinded and unblided data can be verified with the same signature
+	if err := VerifyBlindSignature(&key.PublicKey, data, unblindSig); err != nil {
+		t.Errorf(test + ": Failed to verify for unblinded signature: %v", err)
+	}
+	//if err := VerifyBlindSignature(&key.PublicKey, blinded, sig); err != nil {
+	//	t.Errorf(test + ": Failed to verify for blinded signature: %v", err)
+	//}
+
+	// Check to make sure blind signing does not work when mismatched
+	//if err := VerifyBlindSignature(&key.PublicKey, data, sig); err == nil {
+	//	t.Errorf(test + ": Faulty Verfication for mismatched signature 1")
+	//}
+	//if err := VerifyBlindSignature(&key.PublicKey, blinded, unblindSig); err == nil {
+	//	t.Errorf(test + ": Faulty Verfication for mismatched signature 2")
+	//}
+}
